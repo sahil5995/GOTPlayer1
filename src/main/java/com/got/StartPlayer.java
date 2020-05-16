b1ba9273f2a478c86f1dd8e5b2eae14edaf42377
sahil <sahil.inteq@gmail.com>
2020-05-16 15:09:10 +0200

    Log implementation and refactored server end point

--- src/main/java/com/got/StartPlayer.java
+++ src/main/java/com/got/StartPlayer.java
@@ -1,4 +1,4 @@
-package com.test;
+package com.got;
 
 
 import org.springframework.boot.SpringApplication;
@@ -7,11 +7,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @SpringBootApplication
-public class MainSocket {
+public class StartPlayer {
 
     public static void main(String[] args) {
-
-        SpringApplication.run(MainSocket.class,args);
+        SpringApplication.run(StartPlayer.class, args);
     }
 
     @Bean
